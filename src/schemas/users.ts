3ce29bf8d6b8ca9ccf2598e7157a5c0217f56edb
FacuEM <facundomartinezesc@gmail.com>
2024-06-02 16:53:13 -0300

    add country validator

--- src/schemas/users.ts
+++ src/schemas/users.ts
@@ -1,11 +1,24 @@
 import { z } from "zod";
+import axios from "axios";
+
+const countryCodeValidator = (countryName: string): Promise<boolean> => {
+  return axios
+    .get(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then((response) => response.data.length > 0)
+    .catch(() => false);
+};
 
 const userSchema = z.object({
   name: z.string(),
   email: z.string().email(),
-  country: z.string(),
+  country: z
+    .string()
+    .refine(
+      (countryName) => countryCodeValidator(countryName),
+      "Invalid country name"
+    ),
 });
 
 export const validateUser = (object: any) => {
-  return userSchema.safeParse(object);
+  return userSchema.safeParseAsync(object);
 };
